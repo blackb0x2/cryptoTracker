83219a2be3d030055b842f081ce788b0c763ad38
Mister B <86166683+blackb0x2@users.noreply.github.com>
2023-06-24 09:53:48 -0400

    Create var.go

---
+++ lib/profile/var.go
@@ -0,0 +1,12 @@
+package profile
+
+import lib "cryptoTracker/lib"
+
+type Websocket = lib.Websocket
+
+var (
+	answer                 bool
+	b                      bool
+	n, x                   int
+	name, stream, interval string
+)
